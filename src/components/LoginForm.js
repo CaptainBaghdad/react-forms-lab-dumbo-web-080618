c1a70dddf42ebb39e393893486491ad4c6995dac
Jason Pitts <juniorjr083@gmail.com>
2018-10-11 02:17:32 +0000

    automatically backed up by learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -19,7 +19,7 @@ class LoginForm extends React.Component {
 
   render() {
     return (
-      <form>
+      <form onSubmit={() => this.props.onSubmit}>
         <div>
           <label>
             Username
